fda64917d1011285d8c69a0b79e3bb7a11f2e09d
Artyom Andreyuk <artsiom@threatrix.io>
2021-12-23 18:43:20 +0300

    next c sharp project

--- dotnet/build.sh
+++
@@ -1,1 +0,0 @@
-pwsh -noprofile ./build.ps1 $@
--- dotnet/src/FluentValidation.Tests.AspNetCore/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FluentValidation.Tests.AspNetCore.Controllers {
-	using Microsoft.AspNetCore.Mvc;
-
-	public class HomeController : Controller{
-        public ActionResult Index() {
-            return Content("Test");
-        }
-    }
-}
